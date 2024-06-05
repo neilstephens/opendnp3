fa3209f03f05c67b3f3c5355df4cddbc6814fa74
Neil Stephens <knarl@ratbert.lan>
2024-06-05 17:37:24 +1000

    fix cmake warning

--- deps/exe4cpp.cmake
+++ deps/exe4cpp.cmake
@@ -4,6 +4,7 @@ FetchContent_Declare(
     exe4cpp
     URL      https://github.com/automatak/exe4cpp/archive/fb878a4de598ba9d6e4338afebf83f96e03af1b8.zip
     URL_HASH SHA1=18B141E8CF09DC8D28CC62DD5FA2920670D501BD
+    DOWNLOAD_EXTRACT_TIMESTAMP FALSE
 )
 
 FetchContent_GetProperties(exe4cpp)
--- deps/ser4cpp.cmake
+++ deps/ser4cpp.cmake
@@ -4,6 +4,7 @@ FetchContent_Declare(
     ser4cpp
     URL      https://github.com/automatak/ser4cpp/archive/3c449734dc530a8f465eb0982de29165cc4e23d5.zip
     URL_HASH SHA1=937B759B7CC80180DA26B47037E796B59798A672
+    DOWNLOAD_EXTRACT_TIMESTAMP FALSE
 )
 
 FetchContent_GetProperties(ser4cpp)
